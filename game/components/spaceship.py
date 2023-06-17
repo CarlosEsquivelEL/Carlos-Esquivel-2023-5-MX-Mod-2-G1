be07f023b93c7a66c597d605865d8d72c804876a
CarlosEsquivelEL <136831086+CarlosEsquivelEL@users.noreply.github.com>
2023-06-17 08:35:28 -0600

    Tarea 1

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -10,9 +10,36 @@ class Spaceship(Sprite):
         self.image = SPACESHIP
         self.image = pygame.transform.scale(self.image, (self.image_widht, self.image_height))
         self.rect = self.image.get_rect()
-    
+        self.screen = pygame.display.set_mode((1100, 600))
+        self.rect.x = 300
+        self.rect.y = 500
+        self.speed_x = 5
+        self.speed_y = 5
+        
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
-    def update(sel, keyboard_events):
-        pass
+    def update(self, events):
+
+      if events[pygame.K_LEFT] and events[pygame.K_UP]:
+         self.rect.x -= self.speed_x
+         self.rect.y -= self.speed_y
+      elif events[pygame.K_LEFT] and events[pygame.K_DOWN]:
+         self.rect.x -= self.speed_x
+         self.rect.y += self.speed_y
+      elif events[pygame.K_RIGHT] and events[pygame.K_UP]:
+         self.rect.x += self.speed_x
+         self.rect.y -= self.speed_y
+      elif events[pygame.K_RIGHT] and events[pygame.K_DOWN]:
+         self.rect.x += self.speed_x
+         self.rect.y += self.speed_y
+      elif events[pygame.K_LEFT]:
+         self.rect.x -= self.speed_x
+      elif events[pygame.K_RIGHT]:
+         self.rect.x += self.speed_x
+      elif events[pygame.K_UP]:
+         self.rect.y -= self.speed_y
+      elif events[pygame.K_DOWN]:
+         self.rect.y += self.speed_y
+
+      self.rect.clamp_ip(self.screen.get_rect())
